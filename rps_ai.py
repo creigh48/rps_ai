e17465356f4ad4c127cf1926c71fd8fedf896314
Anand Patel <patela29@students.rowan.edu>
2014-11-20 11:27:12 -0500

    Refactored the move choices to the following: 0 - Rock 1 - Paper 2 - Scissors Fixed some typos and changed location of collect_data calls

--- rps_ai.py
+++ rps_ai.py
@@ -1,10 +1,14 @@
-__author__ = 'jeffrey creighton & anad patel'
+__author__ = 'jeffrey creighton & anand patel'
 import random
+
+#rock 0
+#paper 1
+#scissors 2
 
 
 class RPS_AI:
     def __init__(self):
-        self.paper_goodess = 0
+        self.paper_goodness = 0
         self.rock_goodness = 0
         self.scissor_goodness = 0
         self.pre_smart = 10
@@ -12,38 +16,41 @@ class RPS_AI:
         def move(self, choice):
             return choice
 
-        def chose(self, pre_smart):
+        def choose(self, pre_smart):
             if (pre_smart > 0):
-                choice = random.randint(1, 3)
-                if (choice == 1):
+                choice = random.randint(0, 2)
+                if (choice == 0):
                     choose_rock(self)
-                elif (choice == 2):
+                elif (choice == 1):
                     choose_paper(self)
-                elif (choice == 3):
+                elif (choice == 2):
                     choose_scissors(self)
                 self.pre_smart -= 1
-                collect_data(self)
+                collect_data(self, choice)
             else:
                 if (get_paper_goodness(self) > get_rock_goodness(self) and get_paper_goodness(self) > get_scissor_goodness(self)):
                     choose_paper(self)
+                    collect_data(self, 1)
                 elif (get_rock_goodness(self) > get_paper_goodness(self) and get_rock_goodness(self) > get_scissor_goodness(self)):
                     choose_rock(self)
+                    collect_data(self, 0)
                 else:
                     choose_scissors(self)
+                    collect_data(self, 2)
 
             collect_data(self)
 
         #Param is the opponents last move, assigns to appropriate goodness value
         def collect_data(self, past_move):
-            if(past_move == 0):
-                self.paper_goodess = get_paper_goodness(self) + 1
-            elif(past_move == 1):
-                self.scissor_goodness = get_scissor_goodness(self) + 1
+            if(past_move == 1):
+                self.paper_goodness = get_paper_goodness(self) + 1
             elif(past_move == 2):
+                self.scissor_goodness = get_scissor_goodness(self) + 1
+            elif(past_move == 0):
                 self.rock_goodness = get_rock_goodness(self) + 1
 
         def get_paper_goodness(self):
-            return self.paper_goodess
+            return self.paper_goodness
 
         def get_rock_goodness(self):
             return self.rock_goodness
