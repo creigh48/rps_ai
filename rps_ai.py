05db66861532f5e1f548ada7c7fcb7c0e4c9666d
Anand Patel <patela29@students.rowan.edu>
2014-11-20 12:30:42 -0500

    Swapped the goodness fields with a single array and updated/trimmed the class accordingly.

--- rps_ai.py
+++ rps_ai.py
@@ -8,9 +8,11 @@ import random
 
 class RPS_AI:
     def __init__(self):
-        self.paper_goodness = 0
-        self.rock_goodness = 0
-        self.scissor_goodness = 0
+        self.moves = [0,0,0]
+        #[rock,paper,scissors]
+        #self.paper_goodness = 0
+        #self.rock_goodness = 0
+        #self.scissor_goodness = 0
         self.pre_smart = 10
 
         def move(self, choice):
@@ -19,50 +21,57 @@ class RPS_AI:
         def choose(self, pre_smart):
             if (pre_smart > 0):
                 choice = random.randint(0, 2)
-                if (choice == 0):
-                    choose_rock(self)
-                elif (choice == 1):
-                    choose_paper(self)
-                elif (choice == 2):
-                    choose_scissors(self)
+                move(self,choice)
+                # if (choice == 0):
+                #     choose_rock(self)
+                # elif (choice == 1):
+                #     choose_paper(self)
+                # elif (choice == 2):
+                #     choose_scissors(self)
                 self.pre_smart -= 1
                 collect_data(self, choice)
             else:
-                if (get_paper_goodness(self) > get_rock_goodness(self) and get_paper_goodness(self) > get_scissor_goodness(self)):
-                    choose_paper(self)
-                    collect_data(self, 1)
-                elif (get_rock_goodness(self) > get_paper_goodness(self) and get_rock_goodness(self) > get_scissor_goodness(self)):
-                    choose_rock(self)
-                    collect_data(self, 0)
-                else:
-                    choose_scissors(self)
-                    collect_data(self, 2)
+                choice = (self.moves.index(max(self.moves)))
+                move(self, choice)
+                collect_data(self, choice)
 
-            collect_data(self)
+                # if (get_rock_goodness(self) > get_paper_goodness(self) and get_rock_goodness(self) > get_scissor_goodness(self)):
+                #     choose_rock(self)
+                #     collect_data(self, 0)
+                # elif (get_paper_goodness(self) > get_rock_goodness(self) and get_paper_goodness(self) > get_scissor_goodness(self)):
+                #     choose_paper(self)
+                #     collect_data(self, 1)
+                #
+                # else:
+                #     choose_scissors(self)
+                #     collect_data(self, 2)
+
 
         #Param is the opponents last move, assigns to appropriate goodness value
         def collect_data(self, past_move):
-            if(past_move == 1):
-                self.paper_goodness = get_paper_goodness(self) + 1
-            elif(past_move == 2):
-                self.scissor_goodness = get_scissor_goodness(self) + 1
-            elif(past_move == 0):
-                self.rock_goodness = get_rock_goodness(self) + 1
+            self.moves[past_move] += 1
+            # if(past_move == 0):
+            #     self.rock_goodness = get_rock_goodness(self) + 1
+            # elif(past_move == 1):
+            #     self.paper_goodness = get_paper_goodness(self) + 1
+            # elif(past_move == 2):
+            #     self.scissor_goodness = get_scissor_goodness(self) + 1
 
-        def get_paper_goodness(self):
-            return self.paper_goodness
+        # def get_rock_goodness(self):
+        #     return self.moves[0]
+        #     #return self.rock_goodness
+        # def get_paper_goodness(self):
+        #     return self.moves[1]
+        #     #return self.paper_goodness
+        # def get_scissor_goodness(self):
+        #     return self.moves[2]
+        #     #return self.scissor_goodness
 
-        def get_rock_goodness(self):
-            return self.rock_goodness
-
-        def get_scissor_goodness(self):
-            return self.scissor_goodness
-
-        def choose_rock(self):
-            self.move(self, 0)
-
-        def choose_paper(self):
-            self.move(self, 1)
-
-        def choose_scissors(self):
-            self.move(self, 2)
+        # def choose_rock(self):
+        #     self.move(self, 0)
+        #
+        # def choose_paper(self):
+        #     self.move(self, 1)
+        #
+        # def choose_scissors(self):
+        #     self.move(self, 2)
---
+++ test.py
@@ -0,0 +1,11 @@
+__author__ = 'anand'
+
+import rps_ai.py
+
+
+moves = [19,20,4]
+paper = 20
+rock = 15
+scissors = 4
+
+print(moves.index(max(moves)))
