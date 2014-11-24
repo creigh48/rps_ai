6ada399e283e8f85570f2c13f544505f42380f3c
jeffrey <creigh48@students.rowan.edu>
2014-11-24 18:28:17 -0500

    Made code legible

--- test.py
+++ test.py
@@ -1,11 +1,56 @@
-__author__ = 'anand'
+__author__ = 'anand & jeff'
+import JCAPPlayer
+import Message
 
-import rps_ai.py
+def play():
+    player = JCAPPlayer.JCAPPlayer()
+    round(player)
+
+def round(player):
+    player.notify(Message.Message.get_match_start_message((player,None)))
+    start(player)
+
+    i = 2
+    while(1 != 0 or i!= 1 or i != 2 or i != 25):
+        i = ask()
+        if i == 25:
+            print("new game")
+            new_match(player)
+            #player.reset()
+            print(player.moves)
+            round(player)
+        play = player.play()
+        print("you chose:", translate(i))
+        print("i chose:", translate(play))
+        end_round(player,play,i,2)
+        print(player.moves)
+        print("\n")
+
+def start(player):
+    player.notify(Message.Message.get_round_start_message((player,None)))
+
+def end_round(player,move,move_opp,win):
+    player.notify(Message.Message.get_round_end_message((player,None),(move,move_opp),win))
+
+def new_match(player):
+    player.notify(Message.Message.get_match_end_message((player,None),(2,4)))
+
+def ask():
+    i = input("Press 0 1 or 2 then enter: ")
+    if(int(i) == 25):
+        return int(i)
+    if(int(i) > 2):
+        print("enter a valid number: ")
+        return ask()
+    return int(i)
 
 
-moves = [19,20,4]
-paper = 20
-rock = 15
-scissors = 4
+def translate(x):
+    if x == 0:
+        return "rock"
+    elif x == 1:
+        return "paper"
+    elif x == 2:
+        return "scissors"
 
-print(moves.index(max(moves)))
+play()
